a1d7252146a1a0f84ae22998aeb698114f632de9
Kensaigm <scarroll10@student.cccd.edu>
2015-05-06 21:55:30 -0700

    update for 2015 05 06

---
+++ Assets/Scripts/KillBird.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillBird : MonoBehaviour {
+
+
+
+	public void KillMe()
+	{
+		Destroy (gameObject);
+	}
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ChangeSpeed()
+	{
+		var animator = GetComponent<Animator> ();
+		animator.speed = Random.Range (.1f, 2f);
+	}
+
+
+}
---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickup : MonoBehaviour {
+
+	/// <summary>
+	/// This script expects a collider2d with IsTrigger checked off
+	/// Raises the trigger enter2 d event.
+	/// </summary>
+	/// <param name="collider">Collider.</param>
+
+	void OnTriggerEnter2D (Collider2D collider)
+	{
+		if (collider.gameObject.tag == "Player") 
+		{
+			var gc = GameObject.FindGameObjectWithTag("GameController");
+
+			gc.GetComponent<GameController>().IncrementScore(1);
+
+			Destroy(gameObject);
+		}
+	}
+}
